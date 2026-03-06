9d5e898dab4f44906cb5b3361358e59c5bb41834
Акбулатов Артур Фаилевич <akbulatov_2007@mail.ru>
2026-03-07 01:16:43 +0300

    урааа запушил

--- solutions/sem02/lesson03/task1.py
+++ solutions/sem02/lesson03/task1.py
@@ -8,13 +8,20 @@ class ShapeMismatchError(Exception):
 def sum_arrays_vectorized(
     lhs: np.ndarray,
     rhs: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if lhs.shape != rhs.shape:
+        raise ShapeMismatchError
+    return np.add(rhs, lhs)
 
 
-def compute_poly_vectorized(abscissa: np.ndarray) -> np.ndarray: ...
+def compute_poly_vectorized(abscissa: np.ndarray) -> np.ndarray:
+    return 3 * (abscissa ** 2) + 2 * abscissa + 1
 
 
 def get_mutual_l2_distances_vectorized(
     lhs: np.ndarray,
     rhs: np.ndarray,
-) -> np.ndarray: ...
+) -> np.ndarray:
+    if lhs.shape[1] != rhs.shape[1]:
+        raise ShapeMismatchError
+    return np.sqrt(((lhs[:, np.newaxis, :] - rhs) ** 2).sum(axis=2))
--- solutions/sem02/lesson03/task2.py
+++ solutions/sem02/lesson03/task2.py
@@ -5,15 +5,31 @@ class ShapeMismatchError(Exception):
     pass
 
 
-def convert_from_sphere(
-    distances: np.ndarray,
-    azimuth: np.ndarray,
-    inclination: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
-
-
 def convert_to_sphere(
-    abscissa: np.ndarray,
-    ordinates: np.ndarray,
-    applicates: np.ndarray,
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]: ...
+    xs: np.ndarray,
+    ys: np.ndarray,
+    zs: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    if not (len(xs) == len(ys) == len(zs)):
+        raise ShapeMismatchError
+    
+    r = np.sqrt(xs**2 + ys**2 + zs**2)
+    phi = np.arctan2(ys, xs)
+    alpha = np.arctan2(np.sqrt(xs**2 + ys**2), zs)
+    
+    return (r, phi, alpha)
+
+
+def convert_from_sphere(
+    r: np.ndarray,
+    phi: np.ndarray,
+    alpha: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    if not (len(r) == len(phi) == len(alpha)):
+        raise ShapeMismatchError
+    
+    x = r * np.sin(alpha) * np.cos(phi)
+    y = r * np.sin(alpha) * np.sin(phi)
+    z = r * np.cos(alpha)
+    
+    return (x, y, z)
