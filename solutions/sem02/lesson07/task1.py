86771b8c1df76c2f2566f3c7f3e9a7f72007c1b4
Акбулатов Артур Фаилевич <akbulatov_2007@mail.ru>
2026-04-10 23:52:42 +0300

    uraaa dzzz

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -13,16 +13,43 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    
+    allcolor = "red"
+    plt.style.use("dark_background")
+
+    if abscissa.shape != ordinates.shape:
+        raise ShapeMismatchError
+    
+    if diagram_type not in ("hist", "violin", "box"):
+        raise ValueError
+
+    fig = plt.figure(figsize=(8, 8))
+    grid = plt.GridSpec(4, 4, wspace = 0.2, hspace = 0.2)
+
+    ax_main = fig.add_subplot(grid[1:, :-1])
+    ax_main.scatter(abscissa, ordinates, color = allcolor, alpha = 0.5)
+
+    ax_top = fig.add_subplot(grid[0, :-1], sharex = ax_main)
+    ax_left = fig.add_subplot(grid[1:, -1], sharey = ax_main)
+
+    if diagram_type == "hist":
+        ax_top.hist(abscissa, bins = 50, color = allcolor, density = True, alpha = 0.5)
+        ax_left.hist(ordinates, bins = 50, color = allcolor, density = True, alpha = 0.5, orientation = "horizontal")
+    elif diagram_type == "violin":
+        ax_top.violinplot(abscissa, vert=False)
+        ax_left.violinplot(ordinates, vert=True)
+    elif diagram_type == "box":
+        ax_top.boxplot(abscissa, vert=False)
+        ax_left.boxplot(ordinates, vert=True)
+
+    ax_top.xaxis.set_visible(False)
+    ax_left.yaxis.set_visible(False)
 
 
 if __name__ == "__main__":
     mean = [2, 3]
     cov = [[1, 1], [1, 2]]
-    space = 0.2
 
     abscissa, ordinates = np.random.multivariate_normal(mean, cov, size=1000).T
-
     visualize_diagrams(abscissa, ordinates, "hist")
     plt.show()
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,29 @@
-# ваш код (используйте функции или классы для решения данной задачи)
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+script_dir = os.path.dirname(__file__)
+file_path = os.path.join(script_dir, "data", "medic_data.json")
+with open(file_path, "r") as f:
+    data = json.load(f)
+
+plt.style.use("dark_background")
+stages = ["I", "II", "III", "IV"]
+before_counts = [data["before"].count(s) for s in stages]
+after_counts = [data["after"].count(s) for s in stages]
+
+x = np.arange(4)
+width = 0.35
+
+plt.bar(x - width/2, before_counts, width, label="До", color="red")
+plt.bar(x + width/2, after_counts, width, label="После", color="blue")
+
+plt.xlabel("Степень недостаточности")
+plt.ylabel("Количество пациентов")
+plt.title("Распределение до и после установки импланта")
+plt.xticks(x, stages)
+plt.legend()
+
+plt.savefig("result.png")
+plt.show()
